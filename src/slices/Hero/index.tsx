bc0fdc5e78383dd2d1b94cc9f43b03758df21c77
jonayeds <jonayeeds@gmail.com>
2024-10-04 16:31:25 +0600

    added scroll animation to hero section

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,10 +1,17 @@
+"use client"
+
 import { Bounded } from "@/components/Bounded";
 import Button from "@/components/Button";
 import { TextSplitter } from "@/components/TextSpliter";
 import { asText, Content } from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 /**
  * Props for `Hero`.
  */
@@ -14,10 +21,72 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+  useGSAP(()=>{
+    const intro1 = gsap.timeline()
+    intro1
+    .set(".hero", {opacity:1})
+    .from(".hero-header-word", {
+      scale:3,
+      opacity:0,
+      ease:"power4.in",
+      delay:0.3,
+      stagger:1
+    })
+    .from(".hero-subheading", {
+      opacity:0,
+      y:30,
+      ease:"power4.in",
+    }, "+=0.8")
+    .from(".hero-body", {
+      opacity:0,
+      y:10,
+    })
+    .from(".hero-button", {
+      opacity:0,
+      y:10,
+      duration:0.6
+    })
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger:{
+        trigger:".hero",
+        start:"top top",
+        end:"bottom bottom",
+        scrub:1.5,
+        markers:true,
+      }
+    })
+    scrollTl
+    .fromTo("body", {
+      backgroundColor:"#FDE047"
+    }, {
+      backgroundColor:"#D9F99D",
+      overwrite:"auto",
+    },
+    1
+  )
+  .from(".text-side-heading .split-char",{
+    scale:1.3,
+    opacity:0,
+    y:40,
+    rotate:-25,
+    stagger:0.1,
+    ease:"back.out(3)",
+    duration:0.5,
+  })
+  .from(".text-side-body", {
+    opacity:0,
+    y:20,
+  })
+
+
+
+  })
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="hero opacity-0"
     >
       <div className="grid">
         <div className="grid h-screen place-items-center">
@@ -32,7 +101,6 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             <div className="hero-body text-2xl font-normal text-sky-950">
               <PrismicRichText field={slice.primary.body} />
             </div>
-            {slice.primary.button}
             <Button
               buttonLink={slice.primary.button_link}
               buttonText={slice.primary.button}
